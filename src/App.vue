471d87c49d8782ed37073fb4553fa5175ce942ac
Zhang YingJie <zhangyj@stpass.com>
2023-06-05 14:19:45 +0800

    test: 💍 save

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,15 @@
+<!--
+ * @Author: kevinZzzzzz
+ * @Date: 2023-06-02 10:58:36
+ * @version: 
+ * @LastEditors: kevinZzzzzz
+ * @LastEditTime: 2023-06-05 11:22:50
+ * @Description: Do not Edit
+ * @FilePath: \vue-ts-vite\src\App.vue
+-->
 <template>
   <DefaultLayout>
-    <router-view />
+    <router-view></router-view>
   </DefaultLayout>
 </template>
 
